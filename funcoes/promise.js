1d68497e81dea253104e41d0f79d092334ebab5f
Philipe Allan Almeida <philipe.almeida@outlook.com>
2020-10-06 17:54:00 -0300

    Working with async and await.

--- funcoes/promise.js
+++ funcoes/promise.js
@@ -20,3 +20,49 @@ function esperarPro(tempo = 2000) {
     });
 }
 esperarPro().then(() => esperarPro());
+
+
+function gerarNumerosEntre(min, max, tempo) {
+    if(min > max) [max, min] = [min, max];
+    return new Promise(resolve => {
+       setTimeout(function () {
+           const fator = max - min + 1;
+           const aleatorio = parseInt(Math.random() * fator) + min;
+           resolve(aleatorio)
+       }, tempo)
+    });
+}
+
+gerarNumerosEntre(1,60, 2000).then(console.log);
+
+function gerarVariosNumeros() {
+    return Promise.all([
+        gerarNumerosEntre(1, 60, 4000),
+        gerarNumerosEntre(1, 60, 1000),
+        gerarNumerosEntre(1, 60, 500),
+        gerarNumerosEntre(1, 60, 3000),
+        gerarNumerosEntre(1, 60, 100),
+        gerarNumerosEntre(1, 60, 5500),
+    ])
+}
+
+console.time('promise')
+
+gerarVariosNumeros()
+    .then(console.log)
+    .then(() => {
+        console.timeEnd('promise')
+    })
+
+async function imprimeNumeros() {
+    let numero1 = null;
+    let numero2 = null;
+
+    await gerarNumerosEntre(1, 60, 4000).then(numero => numero1 = numero);
+    console.log(`O valor do numero 1 e: ${numero1}`);
+
+    await gerarNumerosEntre(1, 60, 4000).then(numero => numero2 = numero);
+    console.log(`O valor do numero 2 e: ${numero2}`);
+}
+
+imprimeNumeros();
