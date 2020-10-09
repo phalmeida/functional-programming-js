1f820e61935cd87c18d8a76170bec29dc5e0d0b4
Philipe Allan Almeida <philipe.almeida@outlook.com>
2020-10-08 21:32:59 -0300

    Using promise

--- projeto1/funcoes.js
+++ projeto1/funcoes.js
@@ -2,10 +2,15 @@ const fs = require('fs');
 const path = require('path');
 
 function lerDiretorio(caminho) {
-    return new Promise(resolve => {
-        const concatenarNome = arquivo => path.join(caminho, arquivo);
-        let arquivos = fs.readdirSync(caminho);
-        resolve(arquivos.map(concatenarNome));
+    return new Promise((resolve, reject) => {
+        try {
+            const concatenarNome = arquivo => path.join(caminho, arquivo);
+            let arquivos = fs.readdirSync(caminho);
+            arquivos = resolve(arquivos.map(concatenarNome));
+            resolve(arquivos);
+        } catch (e) {
+            reject(e);
+        }
     })
 }
 
